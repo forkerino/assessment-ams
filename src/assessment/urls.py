8865e80e2660e3c70791a56fc93ffcd3ef4a87d5
Barry Daniels <github@barrydaniels.nl>
2024-11-11 09:05:44 +0100

    assessment typo fix

--- src/assessment/urls.py
+++ src/assessment/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for assesment project.
+URL configuration for assessment project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
