73cb6eea562114919e4d3e55b2116f9933927828
Pieter Stokkink <levensvorm@pm.me>
2024-11-19 12:16:32 +0100

    make user optional

--- src/assessment/geoapi/models.py
+++ src/assessment/geoapi/models.py
@@ -2,7 +2,10 @@ from django.conf import settings
 from django.contrib.gis.db import models as geo_models
 from django.db import models
 
+
 class GeoLocation(models.Model):
     location = geo_models.PointField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True
+    )
