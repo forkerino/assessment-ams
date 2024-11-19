02399aa970151cabc4758a956e9740c11aa64c91
Pieter Stokkink <levensvorm@pm.me>
2024-11-19 15:20:57 +0100

    add filtering based on user

--- src/assessment/geoapi/views.py
+++ src/assessment/geoapi/views.py
@@ -2,6 +2,7 @@ from .models import GeoLocation
 from .serializers import GeoLocationSerializer
 from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.geos import GEOSGeometry
+from django.db.models import Q
 from rest_framework import viewsets
 
 
@@ -10,14 +11,26 @@ class GeoLocationViewset(viewsets.ModelViewSet):
     serializer_class = GeoLocationSerializer
 
     def get_queryset(self):
-        objects = GeoLocation.objects.all()
+        objects = GeoLocation.objects
+        user = self.request.user
+
+        # Anonymous users can only see points without a user
+        if user.is_anonymous:
+            objects = objects.filter(user__isnull=True)
+
+        # Authenticated users can only see their own points, unless they are a superuser
+        if not user.is_superuser and user.is_authenticated:
+            objects = objects.filter(Q(user=user) | Q(user__isnull=True))
+
+        # Filter on lat/lon/(maximum) distance
         params = self.request.query_params
         lat = params.get("lat")
         lon = params.get("lon")
         distance = params.get("distance")
         if distance and lat and lon:
             origin = GEOSGeometry(f"POINT({lon} {lat})", srid=4326)
-            return objects.annotate(
+            objects = objects.annotate(
                 distance=Distance("location", origin, srid=4326)
             ).filter(distance__lte=distance)
-        return objects
+
+        return objects.all()
