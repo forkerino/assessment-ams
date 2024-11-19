7f1ac769b08347fd9e15ad4ae41e374a7173d619
Pieter Stokkink <levensvorm@pm.me>
2024-11-19 14:46:29 +0100

    use drf_extra_fields for the location, default to srid 4326

--- src/assessment/geoapi/serializers.py
+++ src/assessment/geoapi/serializers.py
@@ -1,7 +1,15 @@
 from .models import GeoLocation
+from django.contrib.auth.models import User
+from drf_extra_fields.geo_fields import PointField
 from rest_framework import serializers
 
+
 class GeoLocationSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), required=False, allow_null=True
+    )
+    location = PointField(required=True, srid=4326)
+
     class Meta:
         model = GeoLocation
-        fields = ['location', 'timestamp', 'user']
+        fields = ["id", "user", "location", "timestamp"]
