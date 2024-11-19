410dc3ba463a935c066422192e627c8d75b1daa1
Pieter Stokkink <levensvorm@pm.me>
2024-11-19 11:29:05 +0100

    add GeoLocation Model

--- src/assessment/geoapi/models.py
+++ src/assessment/geoapi/models.py
@@ -1,3 +1,8 @@
+from django.conf import settings
+from django.contrib.gis.db import models as geo_models
 from django.db import models
 
-# Create your models here.
+class GeoLocation(models.Model):
+    location = geo_models.PointField()
+    timestamp = models.DateTimeField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
